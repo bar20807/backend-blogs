76145bb8cf200f319e380e839f683263fb9846bc
Rodrigo Barrera <bar20807@uvg.edu.gt>
2024-02-03 18:07:17 -0600

    backend-blogs

--- app.js
+++ app.js
@@ -3,5 +3,25 @@ const express = require('express')
 const app = express()
 const config = require('./utils/config')
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middelware = require('./utils/middelware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
+// Connect to MongoDB Database
+mongoose.connect(
+    config.MONGODB_URI
+).then(
+    () => logger.info('connected to MongoDB')
+).catch(
+    error => logger.error('error connecting to MongoDB', error.message)
+)
 
+app.use(cors())
+app.use(express.json())
+app.use(middelware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+app.use(middelware.unknownEndpoint)
+app.use(middelware.requestLogger)
+
+module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,16 +2,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (res) => {
-    Blog.find({})
-        .then(blogs => res.json(blogs))
+blogsRouter.get('/', (req, res) => {
+    Blog.find({}).then((blogs) => {
+        res.json(blogs)
+    })
 })
 
 blogsRouter.post('/', (req, res, next) => {
     //La función del controlador de eventos puede acceder a los datos de la propiedad body del objeto de request.
     const newBlog = req.body
     //revisamos si el contenido no esta vacio
-    if (!newBlog.content) {
+    if (!newBlog) {
         //Si a los datos recibidos les falta un valor para la propiedad content, el servidor responderá a la solicitud con el código de estado 400 bad request
         return res.status(400).json({
             error: 'content missing',
@@ -22,7 +23,7 @@ blogsRouter.post('/', (req, res, next) => {
         title: newBlog.title,
         author:  newBlog.author,
         url:  newBlog.url,
-        likes: Number
+        likes: newBlog.likes
     })
 
     blog
